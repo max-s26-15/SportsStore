b7f8a59b1e3cf5fde4ada0538faf9dc241695332
Max <maxsoltys47@gmail.com>
2021-12-14 17:21:15 +0200

    Created a storage class

---
+++ SportsStore/Models/EFProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+
+namespace SportsStore.Models
+{
+    public class EFProductRepository:IProductRepository
+    {
+        private ApplicationDbContext _context;
+
+        public EFProductRepository(ApplicationDbContext ctx)
+        {
+            _context = ctx;
+        }
+
+        public IQueryable<Product> Products => _context.Products;
+    }
+}
