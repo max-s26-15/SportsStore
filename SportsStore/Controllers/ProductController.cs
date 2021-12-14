ace9769dff4467362538351446ab5531658aea61
Max <maxsoltys47@gmail.com>
2021-12-14 15:20:32 +0200

    Created Product Controller in Controllers folder

---
+++ SportsStore/Controllers/ProductController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository _repository;
+
+        public ProductController(IProductRepository repository) => _repository = repository;
+
+        public ViewResult List() => View(_repository.Products);
+    }
+}
