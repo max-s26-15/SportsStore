b79762a21361321f32a77d21a93a017dfa99cb79
Max <maxsoltys47@gmail.com>
2021-12-14 13:37:42 +0200

    Created FakeProductRepository in Models folder

---
+++ SportsStore/Models/FakeProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SportsStore.Models
+{
+    public class FakeProductRepository:IProductRepository
+    {
+        public IQueryable<Product> Products => new List<Product>
+        {
+            new() { Name = "Football net", Price = 25 },
+            new() { Name = "Surf board", Price = 179 },
+            new() { Name = "Running shoes", Price = 95 }
+        }.AsQueryable();
+    }
+}
