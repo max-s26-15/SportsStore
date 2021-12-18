5437ca7c74fba16f5dc3bc054deb3db67daca22b
Max <maxsoltys47@gmail.com>
2021-12-18 14:26:31 +0200

    Generating set of categories

--- SportsStore/Components/NavigationMenuViewComponent.cs
+++ SportsStore/Components/NavigationMenuViewComponent.cs
@@ -1,9 +1,20 @@
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
 
 namespace SportsStore.Components
 {
     public class NavigationMenuViewComponent:ViewComponent
     {
-        public string Invoke() => "Hello from the Nav View Component";
+        private IProductRepository _repository;
+
+        public NavigationMenuViewComponent(IProductRepository repository) => 
+            _repository = repository;
+
+        public IViewComponentResult Invoke() => View(_repository.Products
+            .Select(x => x.Category)
+            .Distinct()
+            .OrderBy(x => x)
+        );
     }
 }
