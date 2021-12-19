e7d159bfd39c0c00db0ac65bb92e0f6a119b55b2
Max <maxsoltys47@gmail.com>
2021-12-19 17:41:16 +0200

    Simplifying the Cart Object

--- SportsStore/Controllers/CartController.cs
+++ SportsStore/Controllers/CartController.cs
@@ -1,35 +1,36 @@
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
-using SportsStore.Infrastructure;
 using SportsStore.Models;
 using SportsStore.Models.ViewModels;
 
 namespace SportsStore.Controllers
 {
-    public class CartController:Controller
+    public class CartController : Controller
     {
         private IProductRepository _repository;
+        private Cart cart;
 
-        public CartController(IProductRepository repository) =>
-            _repository = repository;
+        public CartController(IProductRepository repo, Cart cartService)
+        {
+            _repository = repo;
+            cart = cartService;
+        }
 
-        public ViewResult Index(string returnUrl)=>
-            View(new CartIndexViewModel
+        public ViewResult Index(string returnUrl)
+        {
+            return View(new CartIndexViewModel
             {
-                Cart = GetCart(),
+                Cart = cart,
                 ReturnUrl = returnUrl
             });
+        }
 
         public RedirectToActionResult AddToCart(int productId, string returnUrl)
         {
             Product product = _repository.Products
                 .FirstOrDefault(p => p.ProductId == productId);
-            if (product!=null)
-            {
-                Cart cart = GetCart();
+            if (product != null)
                 cart.AddItem(product, 1);
-                SaveCart(cart);
-            }
 
             return RedirectToAction("Index", new { returnUrl });
         }
@@ -38,25 +39,11 @@ namespace SportsStore.Controllers
         {
             Product product = _repository.Products
                 .FirstOrDefault(p => p.ProductId == productId);
-            if (product!=null)
-            {
-                Cart cart = GetCart();
-                cart.AddItem(product, 1);
-                SaveCart(cart);
-            }
+
+            if (product != null)
+                cart.RemoveLine(product);
 
             return RedirectToAction("Index", new { returnUrl });
-        }
-
-        private Cart GetCart()
-        {
-            Cart cart = HttpContext.Session.GetJson<Cart>("Cart") ?? new Cart();
-            return cart;
-        }
-
-        private void SaveCart(Cart cart)
-        {
-            HttpContext.Session.SetJson("Cart", cart);
         }
     }
 }
