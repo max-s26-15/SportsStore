39e3ef4ba4c29d4adb68f489ea807caeaf2e6a29
Max <maxsoltys47@gmail.com>
2021-12-20 22:51:35 +0200

    Completed building the Order Controller, created tests for OrderController, created displaying plausibility check error messages, displaying the summary page

--- SportsStore/Models/EFOrderRepository.cs
+++ SportsStore/Models/EFOrderRepository.cs
@@ -1,13 +1,16 @@
-using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace SportsStore.Models
 {
-    public class EFOrderRepository:IOrderRepository
+    public class EFOrderRepository : IOrderRepository
     {
         private ApplicationDbContext context;
 
-        public EFOrderRepository(ApplicationDbContext ctx) => context = ctx;
+        public EFOrderRepository(ApplicationDbContext ctx)
+        {
+            context = ctx;
+        }
 
         public IQueryable<Order> Orders => context.Orders
             .Include(o => o.Lines)
@@ -15,9 +18,10 @@ namespace SportsStore.Models
 
         public void SaveOrder(Order order)
         {
-            context.AddRange(order.Lines.Select(l=>l.Product));
+            context.AttachRange(order.Lines.Select(l => l.Product));
             if (order.OrderID == 0)
                 context.Orders.Add(order);
+
             context.SaveChanges();
         }
     }
